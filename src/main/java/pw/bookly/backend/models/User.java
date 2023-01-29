6e0c41de57dedb940934e743d905adcfdf0cfd65
faliszewskii <igor.faliszewski1@gmail.com>
2023-01-29 23:51:35 +0100

    user as identity

--- src/main/java/pw/bookly/backend/models/User.java
+++ src/main/java/pw/bookly/backend/models/User.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column
     private Boolean isActive;
